2bfe947eab215972a3b242ded54d05d75e84bd0b
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2023-05-01 19:17:07 +0530

    added a few tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,32 @@ RSpec.describe User, type: :model do
       expect(user.valid?).to eq(true)
     end
 
+    it "should generate an authentication token" do
+      expect(user.authentication_token).not_to be_nil
+    end
+
+    it "should regenerate auth token when user signs out" do
+      old_token = user.authentication_token
+      user.authentication_token = nil
+      expect(user.authentication_token).not_to eq(old_token)
+    end
+
     it "create admin user" do
       expect(admin_user.isAdmin).to eq(true)
     end
 
   end
+  #
+  # context "test associations" do
+  # it { should have_many(:group_permissions) }
+  # it { should have_many(:posts) }
+  # it { should have_many(:friendships) }
+  # it { should have_many(:comments) }
+  # it { should have_many(:likes) }
+  # it { should have_many(:friend_requests_from).class_name('User').with_foreign_key('friend_request_from_id') }
+  # it { should have_many(:friend_requests_to).class_name('User').with_foreign_key('friend_request_to_id') }
+  # it { should have_and_belong_to_many(:groups) }
+  # end
 
   context "deletion of user" do
     let!(:user) { create(:user) }
@@ -24,25 +45,25 @@ RSpec.describe User, type: :model do
     let!(:post_1) { create(:post, user: user) }
     let!(:post_2) { create(:post, user: user) }
 
-    #creating likes for user
+    # creating likes for user
     let!(:like_1) { create(:like, user: user, post: post_1) }
     let!(:like_2) { create(:like, user: user, post: post_2) }
 
     # creating comments for user
-    let!(:comment_1) { create(:comment, user: user, post: post_1 ) }
-    let!(:comment_2) { create(:comment, user: user, post: post_2 ) }
+    let!(:comment_1) { create(:comment, user: user, post: post_1) }
+    let!(:comment_2) { create(:comment, user: user, post: post_2) }
 
     # create groups for user and add users into it
-    let!(:group_1) { create(:group, user: user, users:[user]) }
-    let!(:group_2) { create(:group, user: user, users:[user]) }
-    let!(:group_3) { create(:group, user: user_1, users:[user, user_1] ) }
+    let!(:group_1) { create(:group, user: user, users: [user]) }
+    let!(:group_2) { create(:group, user: user, users: [user]) }
+    let!(:group_3) { create(:group, user: user_1, users: [user, user_1]) }
 
-    let!(:group_permission_1) { create(:group_permission, group:group_1, user: user, role: :admin)}
-    let!(:group_permission_2) { create(:group_permission, group:group_2, user: user, role: :admin)}
-    let!(:group_permission_3) { create(:group_permission, group:group_3, user: user_1, role: :admin)}
-    let!(:group_permission_4) { create(:group_permission, group:group_3, user: user, role: :member)}
+    let!(:group_permission_1) { create(:group_permission, group: group_1, user: user, role: :admin) }
+    let!(:group_permission_2) { create(:group_permission, group: group_2, user: user, role: :admin) }
+    let!(:group_permission_3) { create(:group_permission, group: group_3, user: user_1, role: :admin) }
+    let!(:group_permission_4) { create(:group_permission, group: group_3, user: user, role: :member) }
 
-    #friendships
+    # friendships
     let!(:friendship_1) { create(:friendship, user: user, friend: user_1) }
 
     it "when user gets deleted" do
@@ -63,7 +84,7 @@ RSpec.describe User, type: :model do
 
   # validation failed specs
   context "validation failure specs" do
-    let(:user) {build(:user)}
+    let(:user) { build(:user) }
     let(:user_1) { build(:user, name: "some other", username: nil) }
     let(:user_2) { build(:user, name: "some other 2", email: user.email) }
     let(:user_3) { build(:user, email: nil) }
