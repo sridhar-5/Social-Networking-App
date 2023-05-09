a64e92262e5080facef922c19b45a8946e1c5b13
rohithmsr <rohit.rajaram@arcadia.com>
2023-05-09 16:49:07 +0530

    Add presence validation to reaction

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,5 +1,6 @@
 class Reaction < ApplicationRecord
   enum reaction_type: {like: 8, funny: 9, congratulate: 10, support: 11, informative: 12}
+  validates :reaction_type, presence: true
   belongs_to :user
   belongs_to :post
 
